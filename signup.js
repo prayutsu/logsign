a0662b2c1701b8658189f6b65327dca9193517a3
prayutsu <54636525+prayutsu@users.noreply.github.com>
2020-02-09 00:47:41 +0530

    addded

---
+++ signup.js
@@ -0,0 +1,8 @@
+
+document.getElementById("signUp").addEventListener('click', () => {
+	document.getElementById("containerk").classList.add("right-panel-active")}
+);
+
+document.getElementById("signIn").addEventListener('click', () => {
+	document.getElementById("containerk").classList.remove("right-panel-active")}
+);
